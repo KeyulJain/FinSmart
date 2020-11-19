04a9b14390fa969de426b0225cfff6b047fca840
KeyulJain <keyuljainkj@gmail.com>
2020-11-19 20:32:00 +0530

    :sparkles: Updated Practical Finance Page

--- js/main.js
+++ js/main.js
@@ -74,11 +74,8 @@ class TypeWriter {
     setTimeout(() => this.type(), typeSpeed);
   }
 }
-
-
 // Init On DOM Load
 document.addEventListener('DOMContentLoaded', init);
-
 // Init App
 function init() {
   const txtElement = document.querySelector('.txt-type');
@@ -86,4 +83,37 @@ function init() {
   const wait = txtElement.getAttribute('data-wait');
   // Init TypeWriter
   new TypeWriter(txtElement, words, wait);
+}
+var principal = 0;
+var interestRate = 0;
+var timesCompounded = 0;
+var termOfLoan = 0;
+var amount = 0;
+
+function simpleInterest() {
+  event.preventDefault();
+  var principal = parseFloat(document.getElementById("principalSimple").value);
+  var interestRate = parseFloat(document.getElementById("interestSimple").value);
+  interestRate = interestRate / 100;
+  var termOfLoan = parseFloat(document.getElementById("termSimple").value);
+  var simpleInt = principal * interestRate * termOfLoan;
+  var amount = (principal + simpleInt).toFixed(2);
+  document.getElementById("siOutput-01").innerHTML = "Interest: $" + simpleInt.toFixed(2);
+  document.getElementById("siOutput-02").innerHTML = "Total plus interest: $" + amount;
+}
+
+function compoundInterest() {
+  event.preventDefault();
+  var principal = parseFloat(document.getElementById("principalCompound").value);
+  var interestRate = parseFloat(document.getElementById("interestCompound").value);
+  interestRate = interestRate / 100;
+  var timesCompounded = parseFloat(document.getElementById("timesCompounded").value);
+  var termOfLoan = parseFloat(document.getElementById("termCompound").value);
+  var a = interestRate / timesCompounded;
+  var b = 1 + a;
+  var c = timesCompounded * termOfLoan;
+  var d = Math.pow(b, c);
+  var amount = (principal * d).toFixed(2);
+  document.getElementById("ciOutput-01").innerHTML = "Interest: ₹" + (amount - principal).toFixed(2);
+  document.getElementById("ciOutput-02").innerHTML = "Total plus interest: ₹" + amount;
 }
